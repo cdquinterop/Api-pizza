ddfd8762edd915911d388e3c6681f9ee7f29ddf9
cdquinterop <darmiqp@gmail.com>
2024-05-21 21:22:10 -0500

    estructura del proyecto

---
+++ src/main/java/com/pizza/apipizza/ApiPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.pizza.apipizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPizzaApplication.class, args);
+	}
+
+}
