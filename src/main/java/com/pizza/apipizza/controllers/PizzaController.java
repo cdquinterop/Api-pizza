e7357025a86953fc6611d32071c3f1e500a86a48
barahona1998MP <marcosplata98@gmail.com>
2024-05-22 22:52:41 -0500

    Create method get and post of Entity Pizza

--- src/main/java/com/pizza/apipizza/controllers/PizzaController.java
+++ src/main/java/com/pizza/apipizza/controllers/PizzaController.java
@@ -1,4 +1,34 @@
 package com.pizza.apipizza.controllers;
 
+import com.pizza.apipizza.dto.PizzaDto;
+import com.pizza.apipizza.repository.PizzaRepository;
+import com.pizza.apipizza.services.PizzaService;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.message.Message;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/pizza")
+@RequiredArgsConstructor
 public class PizzaController {
+
+    private final PizzaService pizzaService;
+
+    @PostMapping
+    public ResponseEntity<PizzaDto> create(@RequestBody PizzaDto pizzaDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(pizzaService.save(pizzaDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PizzaDto>> findAll() {
+        List<PizzaDto> pizzaDto = pizzaService.findAll();
+        if(pizzaDto.isEmpty())
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(pizzaDto);
+    }
+
 }
--- src/main/java/com/pizza/apipizza/services/PizzaService.java
+++ src/main/java/com/pizza/apipizza/services/PizzaService.java
@@ -8,37 +8,66 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 
 @Service
 @RequiredArgsConstructor
 public class PizzaService {
+
     private final PizzaRepository pizzaRepository;
 
-    public PizzaDto save(Pizza pizza) {
-
-        return PizzaDto.builder()
-                .pizzaName(pizza.getPizzaName())
-                .pizzaOrigin(pizza.getPizzaOrigin())
-                .pizzaDescription(pizza.getPizzaDescription())
+    public PizzaDto save(PizzaDto pizzaDto) {
+        Pizza pizza = Pizza.builder()
+                .pizzaName(pizzaDto.getPizzaName())
+                .pizzaOrigin(pizzaDto.getPizzaOrigin())
+                .pizzaDescription(pizzaDto.getPizzaDescription())
                 .build();
+        return convertToDto(pizzaRepository.save(pizza));
     }
 
     public List<PizzaDto> findAll() {
-        return (List) pizzaRepository.findAll();
+        return StreamSupport.stream(pizzaRepository.findAll().spliterator(), false)
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
     }
 
     public PizzaDto findById(Integer id) {
         return pizzaRepository.findById(id)
-                .map(this::mapToDto)
+                .map(this::convertToDto)
                 .orElseThrow(() -> new NoSuchElementException("Id not found"));
     }
 
-    public PizzaDto mapToDto(Pizza pizza) {
+    public Pizza update(Integer id, PizzaDto pizzaDto) {
+        Pizza existingPizzaId= pizzaRepository.findById(id).get();
+        if(existingPizzaId != null) {
+            return Pizza.builder()
+                    .pizzaId(existingPizzaId.getPizzaId())
+                    .pizzaName(pizzaDto.getPizzaName())
+                    .pizzaOrigin(pizzaDto.getPizzaOrigin())
+                    .pizzaDescription(pizzaDto.getPizzaDescription())
+                    .build();
+        } else {
+            return null;
+        }
+    }
+
+    public void delete(Integer id) {
+        pizzaRepository.deleteById(id);
+    }
+
+
+    public PizzaDto convertToDto(Pizza pizza) {
         return PizzaDto.builder()
+                .pizzaId(pizza.getPizzaId())
                 .pizzaName(pizza.getPizzaName())
                 .pizzaOrigin(pizza.getPizzaOrigin())
                 .pizzaDescription(pizza.getPizzaDescription())
                 .build();
     }
+
+
+
+
 }
